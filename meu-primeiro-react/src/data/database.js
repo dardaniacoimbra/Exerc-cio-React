07684b7b8cf5db61b4f98a2ef1a237eb2ef6f053
dardaniacoimbra <dardaniacoimbra@gmail.com>
2021-12-06 23:44:55 -0300

    Exercício de React

---
+++ meu-primeiro-react/src/data/database.js
@@ -0,0 +1,23 @@
+const musicas = [
+
+  {
+    nome: "A Queda - Glória Groove",
+    letra: "Extra! Extra! / Não fique de fora dessa / Garanta seu ingresso pra me ver fazendo merda / Extra! Extra! / Logo logo o show começa / Melhor do que a subida, só mesmo assistir à queda ",
+    imagem: "https://www.ofuxico.com.br/wp-content/uploads/2021/10/gloria-groove-fantasiada-de-arlequim-em-cena-do-clipe-de-a-queda.jpg"
+  },
+  
+  {
+    nome: "Triste, Louca ou Má - Francisco, el Hombre",
+    letra: "Que um homem não te define / Sua casa não te define / Sua carne não te define / Você é seu próprio lar",
+    imagem: "https://studiosol-a.akamaihd.net/tb/letras-blog/wp-content/uploads/2021/03/f0dbc82-francisco-el-hombre-1024x639.jpg"
+  },
+
+  {
+    nome: "Ostentação da Cultura - Tassia Reis & Djonga",
+    letra: "Desfrutando de tudo que eu posso / Minha nobreza é enriquecer os nossos / Se liga como eu tô / Não diga que não notou / Vivendo acima do topo / Não diga que não notou",
+    imagem: "https://portalpopline.com.br/wp-content/uploads/2021/11/Djonga-e-Tassia-Reis-758x451.jpg"
+  },
+
+]
+
+export default musicas
